23d3427521a8a44b1e5ff80c0347011d52db916e
Iván López <lopezi@objectcomputing.com>
2020-07-01 15:53:39 +0200

    Rename project

--- build-native-image.sh
+++ build-native-image.sh
@@ -1,3 +1,3 @@
 ./gradlew assemble
-native-image --no-server --no-fallback --class-path build/libs/function-*-all.jar
+native-image --no-server --no-fallback --class-path build/libs/mn-aws-function-graal-*-all.jar
 zip -j build/function.zip bootstrap mn-aws-function-graal
