5f7acf181602a0987fe70f1a2798f337ab7c6ac5
Iván López <lopezi@objectcomputing.com>
2020-07-13 17:32:42 +0200

    Fail the build without packaging the function if native-image fails

--- build-native-image.sh
+++ build-native-image.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+EXIT_STATUS=0
+
 ./gradlew assemble
-native-image --no-server --no-fallback --class-path build/libs/mn-aws-function-graal-*-all.jar
-zip -j build/function.zip bootstrap mn-aws-function-graal
+native-image --no-server --no-fallback --class-path build/libs/mn-aws-function-graal-*-all.jar || EXIT_STATUS=$?
+if [[ $EXIT_STATUS -eq 0 ]]; then
+  zip -j build/function.zip bootstrap mn-aws-function-graal
+else
+  exit 1
+fi
