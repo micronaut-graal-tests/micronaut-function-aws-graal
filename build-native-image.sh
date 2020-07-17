b33a19d11f9fbd30e767552910962081d66cc766
Iván López <lopezi@objectcomputing.com>
2020-07-17 09:59:58 +0200

    Remove --no-server because now it is the default option

--- build-native-image.sh
+++ build-native-image.sh
@@ -3,7 +3,7 @@
 EXIT_STATUS=0
 
 ./gradlew assemble
-native-image --no-server --no-fallback --class-path build/libs/mn-aws-function-graal-*-all.jar || EXIT_STATUS=$?
+native-image --no-fallback --class-path build/libs/mn-aws-function-graal-*-all.jar || EXIT_STATUS=$?
 if [[ $EXIT_STATUS -eq 0 ]]; then
   zip -j build/function.zip bootstrap mn-aws-function-graal
 else
