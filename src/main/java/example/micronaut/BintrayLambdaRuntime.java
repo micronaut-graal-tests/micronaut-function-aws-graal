5328944c6d07485b4e572b5c3c5c812396cb6f27
Iván López <lopezi@objectcomputing.com>
2021-04-27 12:22:10 +0200

    Use Micronaut `@Nullable` annotation

--- src/main/java/example/micronaut/BintrayLambdaRuntime.java
+++ src/main/java/example/micronaut/BintrayLambdaRuntime.java
@@ -3,9 +3,9 @@ package example.micronaut;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import io.micronaut.core.annotation.Nullable;
 import io.micronaut.function.aws.runtime.AbstractMicronautLambdaRuntime;
 
-import javax.annotation.Nullable;
 import javax.inject.Singleton;
 import java.net.MalformedURLException;
 
