74f7b4da74f6db2ec65117b9bbd37f9f815ff06a
Iván López <lopezi@objectcomputing.com>
2021-11-26 12:26:15 +0100

    Upgrade to Micronaut Gradle Plugin 3.0.0

--- build-native-image.sh
+++ build-native-image.sh
@@ -2,9 +2,9 @@
 
 EXIT_STATUS=0
 
-./gradlew nativeImage || EXIT_STATUS=$?
+./gradlew nativeCompile || EXIT_STATUS=$?
 if [[ $EXIT_STATUS -eq 0 ]]; then
-  zip -j build/function.zip bootstrap build/native-image/application
+  zip -j build/function.zip bootstrap build/native/nativeCompile/mn-aws-function-graal
 else
   exit 1
 fi
